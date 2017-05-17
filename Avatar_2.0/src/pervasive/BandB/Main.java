eedf1dde5d0a9d28d13e1925600d9c88aa20ced0
Dario Bacigalupo <cialan93@gmail.com>
2017-05-17 12:06:15 +0200

    Implemented ultrasonic sensor

--- Avatar_2.0/src/pervasive/BandB/Main.java
+++ Avatar_2.0/src/pervasive/BandB/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import lejos.hardware.Button;
 import lejos.hardware.motor.Motor;
 import lejos.robotics.RegulatedMotor;
+import lejos.robotics.SampleProvider;
 import lejos.robotics.chassis.Chassis;
 import lejos.robotics.chassis.Wheel;
 import lejos.robotics.chassis.WheeledChassis;
@@ -12,29 +13,55 @@ import lejos.robotics.navigation.MoveController;
 import lejos.robotics.navigation.MovePilot;
 import lejos.robotics.navigation.Navigator;
 import lejos.utility.PilotProps;
+import lejos.hardware.Sound;
+import lejos.hardware.ev3.LocalEV3;
+import lejos.hardware.port.*;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.utility.Delay;
 
 public class Main
 {
 	static RegulatedMotor leftMotor;
 	static RegulatedMotor rightMotor;
+	static MovePilot pilot;
+	static EV3UltrasonicSensor UltrasonicSensor;
+	
+	static boolean exit = false; 
 	
 	public static void main(String[] args) throws Exception {
 		
-		MovePilot pilot;
-    	
+		//*****MOTORI & RUOTE
     	Wheel leftWheel = WheeledChassis.modelWheel(Motor.D, 55.0).offset(60).gearRatio(1);
     	Wheel rightWheel = WheeledChassis.modelWheel(Motor.A, 55.0).offset(-60).gearRatio(1);
     	Chassis myChassis = new WheeledChassis( new Wheel[]{leftWheel, rightWheel}, WheeledChassis.TYPE_DIFFERENTIAL);
+    	pilot = new MovePilot(myChassis);     	
+    	pilot.setLinearSpeed(200);
     	
-    	pilot = new MovePilot(myChassis);    
+    	//*****ULTRASUONI
+		UltrasonicSensor = new EV3UltrasonicSensor(SensorPort.S4);
+    	SampleProvider distance = UltrasonicSensor.getMode("Distance");
+    	float[] sample = new float[1];  	
     	
-    	pilot.setAngularSpeed(pilot.getMaxAngularSpeed());
-    	pilot.setLinearSpeed(400);
     	
-    	pilot.travel(300);
-    	pilot.travel(-300);
-    	pilot.rotate(1080);
-    	pilot.rotate(1080);
-
+    	//*****PROGRAMMA
+    	pilot.forward();
+    	while(exit == false)
+    	{
+    		if(Button.DOWN.isDown())
+    			exit = true;
+    		
+    		distance.fetchSample(sample, 0);
+    		System.out.println(sample[0]*100 + "cm");
+    		
+    		if(sample[0]*100.0 < 24.0)
+    		{
+    			pilot.stop();
+    			pilot.rotate(180);
+    			distance.fetchSample(sample, 0);
+    			if(sample[0]*100.0 > 24.0)
+    	    		pilot.forward();
+    			System.out.println(sample[0]*100 + "cm"); 			
+    		}
+    	}   	
 	}
 }
